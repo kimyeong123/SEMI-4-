f55785ea6e84887ccaa06c3799e3c81dce1b6db2
kimyeong123 <dudeh17@naver.com>
2025-09-30 16:52:39 +0900

    최초 프로젝트 생성

---
+++ yesican/src/main/java/com/kh/yesican/YesicanApplication.java
@@ -0,0 +1,13 @@
+package com.kh.yesican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesicanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesicanApplication.class, args);
+	}
+
+}
---
+++ yesican/src/test/java/com/kh/yesican/YesicanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.yesican;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesicanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
