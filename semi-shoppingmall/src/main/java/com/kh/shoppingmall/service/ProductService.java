66c162a74ca1dc2fae85a315fa9478472d84bcea
wantraiseapomeranian <alswns6258@gmail.com>
2025-10-22 12:24:26 +0900

    productService 수정

--- semi-shoppingmall/src/main/java/com/kh/shoppingmall/service/ProductService.java
+++ semi-shoppingmall/src/main/java/com/kh/shoppingmall/service/ProductService.java
@@ -16,128 +16,130 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ProductService {
-    @Autowired
-    private ProductDao productDao;
-    @Autowired
-    private ProductOptionDao productOptionDao;
-    @Autowired
-    private ProductCategoryMapDao productCategoryMapDao;
-    @Autowired
-    private AttachmentService attachmentService;
-    @Autowired
-    private AttachmentDao attachmentDao;
-    
-    // 상품 등록
-    @Transactional
-    public void register(
-        ProductDto productDto,
-        List<ProductOptionDto> optionList,
-        List<Integer> categoryNoList,
-        MultipartFile thumbnailFile,
-        List<MultipartFile> detailImageList
-    ) throws Exception {
-        // 1. 썸네일 저장
-        int thumbnailNo = attachmentService.save(thumbnailFile);
-        productDto.setProductThumbnailNo(thumbnailNo);
+	@Autowired
+	private ProductDao productDao;
+	@Autowired
+	private ProductOptionDao productOptionDao;
+	@Autowired
+	private ProductCategoryMapDao productCategoryMapDao;
+	@Autowired
+	private AttachmentService attachmentService;
+	@Autowired
+	private AttachmentDao attachmentDao;
 
-        // 2. 상품 저장
-        productDao.insert(productDto); // productNo 생성됨
-        int productNo = productDto.getProductNo();
+	// 상품 등록
+	@Transactional
+	public void register(ProductDto productDto, List<ProductOptionDto> optionList, List<Integer> categoryNoList,
+			MultipartFile thumbnailFile, List<MultipartFile> detailImageList) throws Exception {
+		// 1. 썸네일 저장
+		int thumbnailNo = attachmentService.save(thumbnailFile);
+		productDto.setProductThumbnailNo(thumbnailNo);
 
-        // 3. 옵션 저장
-        for (ProductOptionDto option : optionList) {
-            option.setProductNo(productNo);
-            productOptionDao.insert(option);
-        }
+		//2-1. 시퀀스 미리 받아오기 
+		int productNo = productDao.sequence(); 
+		productDto.setProductNo(productNo);
 
-        // 4. 카테고리 매핑 저장
-        for (Integer categoryNo : categoryNoList) {
-            productCategoryMapDao.insert(productNo, categoryNo);
-        }
+		//2-2.상품 저장 (이제 productDto에 productNo가 설정됨)
+		productDao.insert(productDto);
 
-        // 5. 상세 이미지 저장
-        for (MultipartFile imageFile : detailImageList) {
-            int attachmentNo = attachmentService.save(imageFile);
-            attachmentDao.updateProductNo(attachmentNo, productNo);
-        }
-    }
+		// 3. 옵션 저장
+		for (ProductOptionDto option : optionList) {
+			option.setProductNo(productNo);
+			option.setOptionNo(productOptionDao.sequence());
+			productOptionDao.insert(option);
+		}
 
-    // 상품 수정
-    @Transactional
-    public void update(
-        ProductDto productDto,
-        List<ProductOptionDto> newOptionList,
-        List<Integer> newCategoryNoList,
-        MultipartFile newThumbnailFile,
-        List<MultipartFile> newDetailImageList,
-        List<Integer> deleteAttachmentNoList
-    ) throws Exception {
-        int productNo = productDto.getProductNo();
+		// 4. 카테고리 매핑 저장
+		for (Integer categoryNo : categoryNoList) {
+			productCategoryMapDao.insert(productNo, categoryNo);
+		}
 
-        // 1. 썸네일 수정 (파일 있으면 교체)
-        if (newThumbnailFile != null && newThumbnailFile.isEmpty() == false) {
-            int newThumbnailNo = attachmentService.save(newThumbnailFile);
-            productDto.setProductThumbnailNo(newThumbnailNo);
-        }
+		// 5. 상세 이미지 저장
+		for (MultipartFile imageFile : detailImageList) {
+			int attachmentNo = attachmentService.save(imageFile);
+			attachmentDao.updateProductNo(attachmentNo, productNo);
+		}
+	}
 
-        // 2. 상품 정보 수정
-        productDao.update(productDto);
+	// 상품 수정
+	@Transactional
+	public void update(ProductDto productDto, List<ProductOptionDto> newOptionList, List<Integer> newCategoryNoList,
+			MultipartFile newThumbnailFile, List<MultipartFile> newDetailImageList,
+			List<Integer> deleteAttachmentNoList) throws Exception {
+		int productNo = productDto.getProductNo();
+		
+		//기존 썸네일 번호 미리 조회
+		ProductDto currentProduct = productDao.selectOne(productNo);
+		Integer oldThumbnailNo = (currentProduct != null) ? currentProduct.getProductThumbnailNo() : null;
 
-        // 3. 옵션 처리
-        List<ProductOptionDto> oldOptionList = productOptionDao.selectListByProduct(productNo);
-
-        // 기존 옵션 중 삭제할 것 찾아서 삭제
-        for (ProductOptionDto oldOption : oldOptionList) {
-            boolean exists = false;
-            for (ProductOptionDto newOption : newOptionList) {
-                if (oldOption.getOptionNo() == newOption.getOptionNo()) {
-                    exists = true;
-                    break;
-                }
+		// 1. 썸네일 수정 (파일 있으면 교체)
+		if (newThumbnailFile != null && newThumbnailFile.isEmpty() == false) {
+			int newThumbnailNo = attachmentService.save(newThumbnailFile);
+			productDto.setProductThumbnailNo(newThumbnailNo);
+			
+			if (oldThumbnailNo != null && oldThumbnailNo > 0 && oldThumbnailNo != newThumbnailNo) {
+                attachmentService.delete(oldThumbnailNo);
             }
-            if (exists == false) {
-                productOptionDao.delete(oldOption.getOptionNo());
-            }
-        }
+		}
 
-        // 새 옵션들 insert or update
-        for (ProductOptionDto newOption : newOptionList) {
-            newOption.setProductNo(productNo);
-            if (newOption.getOptionNo() == 0) {
-                productOptionDao.insert(newOption);
-            } else {
-                productOptionDao.update(newOption);
-            }
-        }
+		// 2. 상품 정보 수정
+		productDao.update(productDto);
 
-        // 4. 카테고리 처리
-        List<Integer> oldCategoryList = productCategoryMapDao.selectCategoryNosByProductNo(productNo);
+		// 3. 옵션 처리
+		List<ProductOptionDto> oldOptionList = productOptionDao.selectListByProduct(productNo);
 
-        // 삭제할 카테고리
-        for (Integer oldCat : oldCategoryList) {
-            if (newCategoryNoList.contains(oldCat) == false) {
-                productCategoryMapDao.delete(productNo, oldCat);
-            }
-        }
+		// 기존 옵션 중 삭제할 것 찾아서 삭제
+		for (ProductOptionDto oldOption : oldOptionList) {
+			boolean exists = false;
+			for (ProductOptionDto newOption : newOptionList) {
+				if (oldOption.getOptionNo() == newOption.getOptionNo()) {
+					exists = true;
+					break;
+				}
+			}
+			if (exists == false) {
+				productOptionDao.delete(oldOption.getOptionNo());
+			}
+		}
 
-        // 추가할 카테고리
-        for (Integer newCat : newCategoryNoList) {
-            if (oldCategoryList.contains(newCat) == false) {
-                productCategoryMapDao.insert(productNo, newCat);
-            }
-        }
+		// 새 옵션들 insert or update
+		for (ProductOptionDto newOption : newOptionList) {
+			newOption.setProductNo(productNo);
+			if (newOption.getOptionNo() == 0) {
+				productOptionDao.insert(newOption);
+			} else {
+				productOptionDao.update(newOption);
+			}
+		}
 
-        // 5. 상세 이미지 삭제 처리
-        if (deleteAttachmentNoList != null) {
-            for (Integer attachmentNo : deleteAttachmentNoList) {
-                attachmentService.delete(attachmentNo);
-            }
-        }
+		// 4. 카테고리 처리
+		List<Integer> oldCategoryList = productCategoryMapDao.selectCategoryNosByProductNo(productNo);
 
-        // 6. 상세 이미지 새로 저장
-        for (MultipartFile imageFile : newDetailImageList) {
-            int attachmentNo = attachmentService.save(imageFile);
-            attachmentDao.updateProductNo(attachmentNo, productNo);
-        }
-    }
+		// 삭제할 카테고리
+		for (Integer oldCat : oldCategoryList) {
+			if (newCategoryNoList.contains(oldCat) == false) {
+				productCategoryMapDao.delete(productNo, oldCat);
+			}
+		}
+
+		// 추가할 카테고리
+		for (Integer newCat : newCategoryNoList) {
+			if (oldCategoryList.contains(newCat) == false) {
+				productCategoryMapDao.insert(productNo, newCat);
+			}
+		}
+
+		// 5. 상세 이미지 삭제 처리
+		if (deleteAttachmentNoList != null) {
+			for (Integer attachmentNo : deleteAttachmentNoList) {
+				attachmentService.delete(attachmentNo);
+			}
+		}
+
+		// 6. 상세 이미지 새로 저장
+		for (MultipartFile imageFile : newDetailImageList) {
+			int attachmentNo = attachmentService.save(imageFile);
+			attachmentDao.updateProductNo(attachmentNo, productNo);
+		}
+	}
 }
